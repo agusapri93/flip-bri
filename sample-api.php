f7494ff009160cad01a0d649a86d299156ad2a39
agusapri93 <agusapri93@gmail.com>
2018-08-09 16:45:58 +0700

    updated

--- sample-api.php
+++ sample-api.php
@@ -1,5 +1,11 @@
 <?php  
-$capture_image = shell exec("fswebcam -r 1920x1080 image.jpg");
-$read_image = shell_exec("ssocr crop 583 494 456 157 -t 30 image.jpg");
-echo $read_image;
+$a = shell_exec('/bin/bash /var/www/html/webcam.sh 2>&1');
+echo $a;
+
+echo "string";
+
+//$output = shell_exec('/usr/bin/fswebcam -S 1 -r 640x480 -q /var/www/html/foto.jpg');
+//echo $output;
+//shell_exec("ssocr crop 583 494 456 157 -t 30 image.jpg");
+//echo $read_image;
 ?>
