e543f316e46c8e928e5eaee702aaea391b45285d
agusapri93 <agusapri93@gmail.com>
2018-08-13 16:17:44 +0700

    updated : upload image to s3

--- sample-api.php
+++ sample-api.php
@@ -1,11 +1,8 @@
 <?php  
-$a = shell_exec('/bin/bash /var/www/html/webcam.sh 2>&1');
-//echo $a;
-$b = shell_exec("ssocr -t 20 image.jpg");
-echo $b;
+$run_webcam = shell_exec('/bin/bash /var/www/html/webcam.sh 2>&1');
+$display_ssocr = shell_exec("ssocr -t 20 image.jpg");
+$upload_s3 = shell_exec('/bin/bash /var/www/html/upload_s3.sh 2>&1');
 
-//$output = shell_exec('/usr/bin/fswebcam -S 1 -r 640x480 -q /var/www/html/foto.jpg');
-//echo $output;
-//shell_exec("ssocr crop 583 494 456 157 -t 30 image.jpg");
-//echo $read_image;
+echo $display_ssocr;
+
 ?>
