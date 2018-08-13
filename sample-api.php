9342e0c297f10636b94ad771250705da210dd576
agusapri93 <agusapri93@gmail.com>
2018-08-13 10:45:43 +0700

    updated 13/8/2018

--- sample-api.php
+++ sample-api.php
@@ -1,8 +1,8 @@
 <?php  
 $a = shell_exec('/bin/bash /var/www/html/webcam.sh 2>&1');
-echo $a;
-
-echo "string";
+//echo $a;
+$b = shell_exec("ssocr -t 20 image.jpg");
+echo $b;
 
 //$output = shell_exec('/usr/bin/fswebcam -S 1 -r 640x480 -q /var/www/html/foto.jpg');
 //echo $output;
--- webcam.sh
+++ webcam.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-fswebcam -r 1280x720 --no-banner /var/www/html/image.jpg
+fswebcam --no-banner -r 1920x1080 --crop 500x180,550x500 /var/www/html/image.jpg
+
+#!fswebcam -r 1920x1080 --no-banner /var/www/html/image.jpg
