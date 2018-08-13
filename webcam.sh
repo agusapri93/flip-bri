889640035d1115c5e3caa08db3b88a031e6bd4e1
agusapri93 <agusapri93@gmail.com>
2018-08-13 11:40:23 +0700

    updated : fix resolution

--- webcam.sh
+++ webcam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-fswebcam --no-banner -r 1920x1080 --crop 500x180,550x500 /var/www/html/image.jpg
+fswebcam --no-banner -r 1920x1080 --crop 500x155,550x500 /var/www/html/image.jpg
 
 #!fswebcam -r 1920x1080 --no-banner /var/www/html/image.jpg
